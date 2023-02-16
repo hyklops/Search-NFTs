da3c9c56ac91ccc4a460f8361e139172d73612e8
Emin Çoşkun <hyklops@hotmail.com>
2023-02-16 19:13:44 +0300

    Add navbar, search function

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,64 @@
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import Navbar from "../components/Navbar";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [goerli],
+  [publicProvider()]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: "NFT Marketplace",
+  chains,
+});
+
+const demoAppInfo = {
+  appName: "NFT Marketplace",
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      argentWallet({ chains }),
+      trustWallet({ chains }),
+      ledgerWallet({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+console.log(webSocketProvider);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+        <Navbar />
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 export default MyApp;
---
+++ pages/search.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { router, useRouter } from "next/router";
+
+const Search = () => {
+  const [NFTs, setNFTs] = useState([]);
+
+  const router = useRouter();
+  const {
+    query: { searchWallet },
+  } = router;
+  const props = {
+    searchWallet,
+  };
+
+  const fetchNFTs = async () => {
+    const response = await fetch(
+      `https://api.opensea.io/api/v1/assets?owner=${props.searchWallet}`
+    ).then((response) => response.json());
+    setNFTs(response);
+  };
+
+  useEffect(() => {
+    fetchNFTs();
+  }, [props.searchWallet]);
+
+  const nftCard = () => {
+    return NFTs?.assets?.map((nft) => {
+      return (
+        <div
+          key={nft.id}
+          className="w-80 max-h-96 rounded overflow-hidden shadow-lg"
+        >
+          <div className="w-80 h-80 mx-auto ">
+            <img
+              className=" mx-auto object-contain h-full w-full"
+              src={nft.image_url}
+              alt="nft"
+            />
+          </div>
+          <div className="mx-5 my-1">
+            <p className="overflow-hidden whitespace-nowrap text-ellipsis">
+              {nft.name}
+            </p>
+            <p className="overflow-hidden whitespace-nowrap text-ellipsis font-semibold">
+              {nft.collection.name}
+            </p>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="">
+      <h1 className="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl "></h1>
+      <div className="flex mt-10 flex-col items-center justify-center mb-5"></div>
+      {NFTs ? (
+        <div className="flex mt-10 flex-wrap gap-6 justify-center">
+          {nftCard()}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Search;
